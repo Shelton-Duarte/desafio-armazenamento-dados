20c8e6489896983d0cf87934d49d35bd201b8ac4
Shelton-Duarte <duarteshelton81@gmail.com>
2024-07-26 13:16:07 +0200

    update:update index

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const connectDB = require("./connect/database");
 
+const port = process.env.PORT || 8000;
 
-const port = process.env.PORT;
+connectDB();
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/users", require("./routes/user-route"));
+app.use("/api/login", require("./routes/login-route"));
+app.use("/api/signUp", require("./routes/signUp-route"));
+
+app.listen(port, () => console.log(`Aplicação rodando em http://localhost:${port}`));
